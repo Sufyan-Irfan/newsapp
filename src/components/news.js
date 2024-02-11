0a8449a5136398b24f3e53d1e92ecaf87f10a25c
Sufyan-Irfan <sufyanirfan0321@gmail.com>
2024-02-11 12:08:00 +0500

    first commit

---
+++ src/components/news.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class news extends Component {
+  render() {
+    return (
+      <div>news</div>
+    )
+  }
+}
+
+export default news
---
+++ src/components/newsItems.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class newsItems extends Component {
+  render() {
+    return (
+      <div>newsItems</div>
+    )
+  }
+}
+
+export default newsItems
